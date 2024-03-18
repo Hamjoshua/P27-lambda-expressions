d9a3880e6d1096cda41014e744ee05d0fcabba7f
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2024-03-18 10:27:42 +0700

    binaryTree обновлено

--- Program.cs
+++ Program.cs
@@ -8,70 +8,77 @@ namespace app
 {
     public class LambdaExpressions
     {
+
+        public class BinaryTree<Type>
+        {
+            public BinaryTreeNode<Type> Root = null;
+
+            public void Add(Type value)
+            {
+
+            }
+            public void Add(Type value)
+            {
+
+            }
+        }
         public class BinaryTreeNode<Type>
         {
             public Type Key;
-            public BinaryTreeNode<Type> Right;
-            public BinaryTreeNode<Type> Left;
-            private Type key;
+            private BinaryTreeNode<Type> _right = null;
+            private BinaryTreeNode<Type> _left = null;
+            public BinaryTreeNode<Type> Right
+            {
+                get
+                {
+                    return _right;
+                }
+                set
+                {
+                    _right = value;
+                    if (value != null)
+                    {
+                        value.Parent = this;
+                    }
+                }
+            }
+            public BinaryTreeNode<Type> Left
+            {
+                get
+                {
+                    return _left;
+                }
+                set
+                {
+                    _left = value;
+                    if (value != null)
+                    {
+                        value.Parent = this;
+                    }
+                }
+            }
+
+            public BinaryTreeNode<Type> Parent;
 
             public BinaryTreeNode(Type key)
             {
                 Key = key;
             }
-        }
-        class BinaryTree<Type> : IEnumerable<Type>
-        {
-            public BinaryTreeNode<Type> Root;
 
-            // todo
-            public BinaryTree(Type key)
+            public BinaryTreeNode(Type key, BinaryTreeNode<Type> parentNode)
             {
-                Root = new BinaryTreeNode<Type>(key);
-            }
-
-            public Type Current
-            {
-                get
-                {
-                    return _currentTree.Key;
-                }
-            }
-
-            public bool Next()
-            {
-                if (SubTree.Item2 == null)
-                {
-                    if ()
-                }
-                return true;
-            }
-
-            public bool Previous()
-            {
-                if (_keySet.Count == 0)
-                {
-                    return false;
-                }
-                _keySet.RemoveAt(_keySet.Count);
-                return true;
-            }
-
-            public IEnumerator GetEnumerator()
-            {
-                return SubTree;
-            }
-
-
-            public void Reset()
-            {
-                throw new NotImplementedException();
+                Key = key;
+                Parent = parentNode;
             }
         }
 
         static void Main(string[] args)
         {
-
+            var root = new BinaryTreeNode<int>(1);
+            var left = new BinaryTreeNode<int>(2);
+            var right = new BinaryTreeNode<int>(2);
+            root.Right = right;
+            root.Left = left;
         }
     }
 }
